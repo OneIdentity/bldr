881e38a5d452f7e5fbd1cff35ae61ba74ae7d0d1
Ferenc Wágner <ferenc.wagner@balabit.com>
2022-02-09 21:06:48 +0100

    New test for modified 3.0 (quilt) packages without actual patches
    Local modifications used to break the build of such packages, because
    we tried to write the corresponding auto-generated patch file into the
    missing debian/patches directory.

--- test/integration/test_git_package_content.py
+++ test/integration/test_git_package_content.py
@@ -8,13 +8,16 @@ from ..testutil import extract_deb
 
 
 @pytest.mark.parametrize(
-    'git_project_name, content_checks',
-    [pytest.param('test-quilt-proj-onedir',
+    'git_project_name, git_project_master_changes, content_checks',
+    [pytest.param('test-quilt-proj-onedir', False,
                   [('usr/share/doc/feeling/alone', "Hello, friend!\n"),
                    ('usr/share/doc/feeling/lonely', "I am a test.\n")],
                   id='onedir'),
+     pytest.param('quilt-no-patch', True,
+                  [('usr/share/doc/quilt-no-patch/README', "Hello patched world!\n")],
+                  id='no_patch'),
     ],
-    indirect=["git_project_name"])
+    indirect=['git_project_name', 'git_project_master_changes'])
 def test_git_package_content(local_repo_dir: Path, git_import: Path, docker_from: str,
                              tmp_path: Path, content_checks: List[Tuple[Path, str]]):
     bldr = BLDR(
