4a9b5c43190cff19114b72111e7dc929f3dd7fb2
Ferenc Wágner <ferenc.wagner@balabit.com>
2022-03-03 11:43:43 +0100

    Parametrize (generalize) simple file content checks
    The quilt_project_path test fixture did not do anything quilt-
    specific, so pass in the project name through the request parameter to
    make it (almost) reusable, and rename it to git_import, because that's
    all it does.  Give the internal variables more generic names as well.
    
    Consequently, parametrize the test with the project name and the file
    data to check, and request the git_import fixture passing through the
    former.
    
    Finally, rename the test source the reflect its widened applicability.

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -1,6 +1,10 @@
-
 import os
 import pytest
+import subprocess
+from pathlib import Path
+from typing import Mapping
+
+from ..testutil import copytree
 
 
 @pytest.fixture()
@@ -12,3 +16,27 @@ def git_env():
     env["GIT_AUTHOR_EMAIL"] = env["GIT_COMMITTER_NAME"]
 
     yield env
+
+
+@pytest.fixture
+def git_import(request,
+               tmp_path: Path, asset_dir: Path, git_env: Mapping[str, str]) -> Path:
+    project = request.param
+    import_from = asset_dir.joinpath(project)
+    repo_dir = tmp_path.joinpath(project)
+    repo_dir.mkdir()
+    subprocess.check_call(['git', 'init'], cwd=repo_dir)
+    subprocess.check_call(['git', 'checkout', '-b', 'upstream'], cwd=repo_dir)
+
+    copytree(import_from.joinpath('upstream'), repo_dir)
+    subprocess.check_call(['git', 'add', '--all'], cwd=repo_dir)
+    subprocess.check_call(['git', 'commit', '--no-verify', '--message', 'Imported upstream'], cwd=repo_dir, env=git_env)
+
+    subprocess.check_call(['git', 'checkout', '-b', 'ubuntu'], cwd=repo_dir)
+    copytree(import_from.joinpath('debian'), repo_dir)
+    subprocess.check_call(['git', 'add', '--all'], cwd=repo_dir)
+    subprocess.check_call(['git', 'commit', '--no-verify', '--message', 'Imported debian'], cwd=repo_dir, env=git_env)
+
+    subprocess.check_call(['git', 'checkout', '-b', 'master'], cwd=repo_dir)
+
+    return repo_dir
---
+++ test/integration/test_git_package_content.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from typing import List, Tuple
+
+import pytest
+
+from bldr.bldr import BLDR
+from ..testutil import extract_deb
+
+
+@pytest.mark.parametrize(
+    'git_import, content_checks',
+    [pytest.param('test-quilt-proj-onedir',
+                  [('usr/share/doc/feeling/alone', "Hello, friend!\n"),
+                   ('usr/share/doc/feeling/lonely', "I am a test.\n")],
+                  id='onedir'),
+    ],
+    indirect=["git_import"])
+def test_git_package_content(local_repo_dir: Path, git_import: Path, docker_from: str,
+                             tmp_path: Path, content_checks: List[Tuple[Path, str]]):
+    bldr = BLDR(
+        local_repo_dir=local_repo_dir,
+        source_dir=git_import.parent,
+        docker_from=docker_from,
+    )
+    bldr.build(git_import)
+
+    deb_file = list(local_repo_dir.glob('**/*.deb'))[0]
+
+    extract_dir = tmp_path.joinpath('extracted')
+    extract_dir.mkdir()
+    extract_deb(deb_file, extract_dir)
+
+    for path, expected in content_checks:
+        assert extract_dir.joinpath(path).read_text() == expected, "File content should be correct."
